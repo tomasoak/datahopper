d852d5c77887c97242ce60bd72471481b2161a67
tomasoak <tomas.jpeg@gmail.com>
2022-04-06 22:23:41 +0200

    normalize_column_string and clean_string

--- ds_toolkit/manager/data_normalize.py
+++ ds_toolkit/manager/data_normalize.py
@@ -3,46 +3,76 @@ from unidecode import unidecode
 import pandas as pd
 
 
-def normalize_string(text: str, case=None, clean=False):
-    """Normalize string characters encoding to UTF-8.
+def normalize_column_string(
+        df: pd.DataFrame, col: str, case=None, clean=False):
+    """
+    Adjust column value characters encoding to UTF-8.
+    Case parameter makes your column string upper, lower or titlecase
+    If clean:
+        Removes special characters such as whitespace, ".", "-", "/", ",", '"'
+    Example:
+        df = pd.DataFrame(columns=["id", "name"],
+                      data=[[1, "john coltRanE-"],
+                            [2, "eLLa FiTzgeralD"],
+                            [3, "MiLes DaviS"]])
+        df = normalize_string(df, name, case="title", clean=True)
+
+        `df = pd.DataFrame(columns=["id",  "name"],
+                        data=[[1,   "John Coltrane"],
+                              [2, "Ella Fitzgerald"],
+                              [3, "Miles Davis"]])`
+    Args:
+      df: pd.DataFrame:
+      col: str:
+      case: (Default value = None)
+      clean: (Default value = False)
+
+    Returns:
+      df = pd.DataFrame: cleaned string column values
+    """
+    df[col] = (
+        df[col]
+        .str.normalize("NFKD")
+        .str.encode("ascii", errors="ignore")
+        .str.decode("utf-8")
+    )
+    if case is None:
+        df[col] = df[col]
+    elif case == "lower":
+        df[col] = df[col].str.lower()
+    elif case == "upper":
+        df[col] = df[col].str.upper()
+    elif case == "title":
+        df[col] = df[col].str.title()
+    else:
+        raise ValueError(
+            "The chosen case must be 'lower', 'upper' or 'title'.")
+
+    if clean is True:
+        df[col] = (
+            df[col]
+            .str.replace(".", "")
+            .str.replace("-", "")
+            .str.replace("/", "")
+            .str.replace(",", "")
+            .str.replace('"', "")
+        )
+    else:
+        df[col] = df[col]
+
+    return df
+
+
+def clean_string(text: str):
+    """ 
+    Take a string and clean it!
 
     - Remove double-whitespace
     - Remove tab, newline, return, formfeed, etc.
     - Replace accented characters (e.g. ö becomes o)
     - Trim leading and trailing whitespace
-
-    Case parameter makes your column string upper, lower or titlecase
-    If clean:
-        Removes special characters such as whitespace, ".", "-", "/", ",", '"'
-
-    Example:
-        df = pd.DataFrame(
-            columns=["id",  "name"],
-            data=[  [  1,   "john coltRanE-"],
-                    [  2,   "eLLa _FiTzgeralD"],
-                    [  3,   "MiLes DaviS"]])
-        df = df["name"].apply(normalize_string, case="title", clean=True)
-        `df = pd.DataFrame(
-            columns=["id",  "name"],
-            data=[  [  1,   "John Coltrane"],
-            [  2,   "Ella Fitzgerald"],
-            [  3,   "Miles Davis"]])`
-
-    Args:
-      text: string
-      case: (Default value = None)
-      clean: (Default value = False)
-
-    Returns:
-      text = string: cleaned string
+    - Convert to upper-case
     """
-
-    text = (
-        text
-        .str.normalize("NFKD")
-        .str.encode("ascii", errors="ignore")
-        .str.decode("utf-8")
-    )
 
     def keep(character):
         category = unicodedata.category(character)
@@ -51,30 +81,10 @@ def normalize_string(text: str, case=None, clean=False):
             and category != "Zl"  # ignore line separator
             and category != "Zp"  # ignore paragraph separator
         )
+
     text = "".join(c for c in text if keep(c))
     text = " ".join(text.split())
     text = unidecode(text)
-
-    if clean:
-        text = (
-            text.replace(".", "")
-            .replace("-", "")
-            .replace("/", "")
-            .replace(",", "")
-            .replace('"', "")
-        )
-
-    if case == "lower":
-        text.lower()
-    elif case == "upper":
-        text.upper()
-    elif case == "title":
-        text.title()
-    else:
-        raise ValueError(
-            "The chosen case must be 'lower', 'upper' or 'title'."
-        )
-
     return text
 
 
