633e9cd3c5cfadfc8a38ba210253a3d2f8574503
tomasoak <tomas.jpeg@gmail.com>
2022-04-06 22:23:54 +0200

    rename test

--- tests/manager/test_data_normalize.py
+++ tests/manager/test_data_normalize.py
@@ -1,17 +1,17 @@
 import pandas as pd
-from ds_toolkit.manager.data_normalize import normalize_string
+from ds_toolkit.manager.data_normalize import normalize_column_string
 
 
-def test_normalize_string():
-  df = pd.DataFrame(columns=["id", "name"],
-            data=[[1,"john coltRanE-"],
-                    [2,"eLLa FiTzgeralD"],
-                    [3,"MiLes DaviS"]])
+def test_normalize_column_string():
+    df = pd.DataFrame(columns=["id", "name"],
+                      data=[[1, "john coltRanE-"],
+                            [2, "eLLa FiTzgeralD"],
+                            [3, "MiLes DaviS"]])
+    df_clean = pd.DataFrame(columns=["id", "name"],
+                            data=[[1, "John Coltrane"],
+                                  [2, "Ella Fitzgerald"],
+                                  [3, "Miles Davis"]])
 
-  df_clean = pd.DataFrame(columns=["id","name"],
-            data=[[1, "John Coltrane"],
-                    [2, "Ella Fitzgerald"],
-                    [3, "Miles Davis"]])             
+    df = normalize_column_string(df, "name", case="title", clean=True)
 
-  df = normalize_string(df, "name", case="title", clean=True)
-  assert all(df["name"] == df_clean["name"])
+    assert all(df["name"] == df_clean["name"])
