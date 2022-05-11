1485f225d641777a6f2ee84913e9e92c4d6a8ebd
tomasoak <tomas.jpeg@gmail.com>
2022-05-11 20:55:58 +0200

    change package name and fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-  author="Tomas Carvalho",
-  description="Complete package for data wrangling in python.",
-  name="ds_toolkit",
-  version="0.1.0",
-  python_requires=">=3.8.*",
-  packages=find_packages(include=["ds_toolkit", "ds_toolkit.*"],
-  install_requires=["pandas>=1.4", "numpy>=1.22"])
+    name="Data Hopper",
+    version="0.1.0",
+    description="Package for data wrangling in python.",
+    license='MIT',
+    author="Tomas Carvalho",
+    author_email="tomas.jpeg@gmail.com",
+    url="https://github.com/tomasoak/data_hopper",
+    python_requires=">=3.8.*",
+    packages=find_packages(include=["data_hopper", "data_hopper.*"]),
+    install_requires=["pandas>=1.4", "numpy>=1.22"]
 )
