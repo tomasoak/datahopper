7068efac96957b294ec018211571f16bb247df28
tomasoak <tomas.jpeg@gmail.com>
2022-05-11 21:28:13 +0200

    release version 0.1.0 on Pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Data Hopper",
+    name="data_hopper",
     version="0.1.0",
     description="Package for data wrangling in python.",
     license='MIT',
