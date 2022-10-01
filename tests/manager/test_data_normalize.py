b38e9d5e35855c192dca8c2cd35b5aa7698cd861
tomasoak <tomas.jpeg@gmail.com>
2022-10-01 09:36:35 +0200

    fix import path

--- tests/manager/test_data_normalize.py
+++ tests/manager/test_data_normalize.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from datahopper.manager.data_normalize import (
+
+from datahopper.manager.normalize import (
     clean_string,
+    drop_rows_missing_values,
     rename_column,
-    drop_rows_missing_values
 )
 
 
@@ -14,34 +15,37 @@ def test_clean_string():
     string_special_cleaned = "Zimbabwe"
 
     assert clean_string(string) == string_cleaned
-    assert clean_string(
-        string_special, especial_char=True) == string_special_cleaned
+    assert clean_string(string_special, especial_char=True) == string_special_cleaned
 
 
 def test_rename_column():
     column_names = ["id", "municipality", "country", "continent"]
 
-    df = pd.DataFrame(
-        columns=["ID", "MUNCIPAL", "COUNTRI", "CONSTINENNT"])
+    df = pd.DataFrame(columns=["ID", "MUNCIPAL", "COUNTRI", "CONSTINENNT"])
 
     aimed_column_names = dict(zip(df.columns, column_names))
 
-    df_renamed = pd.DataFrame(
-        columns=["id", "municipality", "country", "continent"])
+    df_renamed = pd.DataFrame(columns=["id", "municipality", "country", "continent"])
 
     assert all(rename_column(df, aimed_column_names) == df_renamed)
 
 
 def test_drop_rows_missing_values():
-    df = pd.DataFrame(columns=["id", "name", "age", "gender"],
-                      data=[[1, "Alex", 32, "Non-Binary"],
-                            [2, "", 28, "Male"],
-                            [3, "Austin", 57, "Male"],
-                            [4, "Palema", "NA", "NA"]])
+    df = pd.DataFrame(
+        columns=["id", "name", "age", "gender"],
+        data=[
+            [1, "Alex", 32, "Non-Binary"],
+            [2, "", 28, "Male"],
+            [3, "Austin", 57, "Male"],
+            [4, "Palema", "NA", "NA"],
+        ],
+    )
 
-    df_cleaned = pd.DataFrame(columns=["id", "name", "age", "gender"],
-                              data=[[1, "Alex", 32, "Non-Binary"],
-                                    [3, "Austin", 57, "Male"]])
+    df_cleaned = pd.DataFrame(
+        columns=["id", "name", "age", "gender"],
+        data=[[1, "Alex", 32, "Non-Binary"], [3, "Austin", 57, "Male"]],
+    )
 
-    assert all(drop_rows_missing_values(
-        df, df.columns).sort_index(inplace=True) == df_cleaned)
+    assert all(
+        drop_rows_missing_values(df, df.columns).sort_index(inplace=True) == df_cleaned
+    )
