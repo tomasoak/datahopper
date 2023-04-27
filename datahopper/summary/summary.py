6cbbbff34eca55e79e8e2dc2f37725e3f1a5ab12
tomasoak <tomas.jpeg@gmail.com>
2023-04-27 16:05:32 +0200

    add  function and test

--- datahopper/summary/summary.py
+++ datahopper/summary/summary.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import numpy as np
 
 
-def summary(data, groupby_col, numeric_col):
+def summary(data: pd.DataFrame, groupby_col: str, numeric_col: str):
     """ Summarizes dataframe based on numeric column
     Gives the percentage of each attribute in which it will be groupped
     and cumulative percentage contribution of given attribute.
 
     Args:
-      data: pd.DataFrame:
+      data: pd.DataFrame
       groupby_col: string
       numeric_col: string
 
@@ -43,7 +44,7 @@ def check(df: pd.DataFrame):
     """Check missing data in a given pandas Dataframe
 
     Args:
-      df: pd.DataFrame:
+      df: pd.DataFrame
 
     Returns:
       df_check: pd.Dataframe
@@ -57,37 +58,44 @@ def check(df: pd.DataFrame):
                              "value_exist": not_null,
                              "total_missing": total_missing,
                              "missing_percentage": missing_percentage})
-    print("Total Data Missing:", total_missing)
-    print("Percentual Data Missing:", missing_percentage)
 
     return df_check
 
 
-def row_difference(df: pd.DataFrame, vol: str):
+def row_difference(df: pd.DataFrame, id_col: str, temporal_col: str, num_col: str):
     """Checks whether the previous values is equal to the current year,
-    if so `CAP_DIFF` is False otherwise, True"""
-    import numpy as np
-    
-    df = df.sort_values(["TRASE_ID", "YEAR"])
+    if so `difference` is False otherwise, True
 
-    previous_value = df[vol].shift(1)
-    
-    unique_trase_id = sorted(df["TRASE_ID"].unique())
-    unique_years = sorted(df["YEAR"].unique())
-    
-    first_year = unique_years[0]
-    not_first_year = df["YEAR"] != first_year
-    
-    bigger_previous = df[vol] > previous_value
-    equal_previous = df[vol] == previous_value
-    lower_previous = df[vol] < previous_value
-    
-    for i, j in zip(unique_trase_id, unique_years):
-        df.loc[df["YEAR"] == first_year, "AREA_TREND"] = np.NaN
-        df.loc[(not_first_year) & (bigger_previous), "AREA_TREND"] = "Increased"
-        df.loc[(not_first_year) & (bigger_previous), "AREA_DIFF"] = df[vol] - previous_value
-        df.loc[equal_previous, "AREA_TREND"] = "Equal"
-        df.loc[(not_first_year) & (lower_previous), "AREA_TREND"] = "Decreased"
-        df.loc[(not_first_year) & (lower_previous), "AREA_DIFF"] = df[vol] - previous_value
-    
+    Args:
+      df: pd.DataFrame
+      id_col: Column with the unique identifier code
+      temporal_col: Column with the timestamp, might be year, month, day
+      num_col: Numerical column with the value aimed to be look at
+
+    Returns:
+      df: pd.Dataframe
+    """
+
+    df = df.sort_values([id_col, temporal_col])
+
+    previous_value = df[num_col].shift(1)
+
+    unique_id = sorted(df[id_col].unique())
+    unique_time = sorted(df[temporal_col].unique())
+
+    first_year = unique_time[0]
+    not_first_year = df[temporal_col] != first_year
+
+    bigger_previous = df[num_col] > previous_value
+    equal_previous = df[num_col] == previous_value
+    lower_previous = df[num_col] < previous_value
+
+    for i, j in zip(unique_id, unique_time):
+        df.loc[df[temporal_col] == first_year, "trend"] = np.NaN
+        df.loc[(not_first_year) & (bigger_previous), "trend"] = "Increased"
+        df.loc[(not_first_year) & (bigger_previous), "difference"] = df[num_col] - previous_value
+        df.loc[equal_previous, "trend"] = "Equal"
+        df.loc[(not_first_year) & (lower_previous), "trend"] = "Decreased"
+        df.loc[(not_first_year) & (lower_previous), "difference"] = df[num_col] - previous_value
+
     return df
